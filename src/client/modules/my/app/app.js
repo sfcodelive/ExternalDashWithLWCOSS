c78f019b94589bbbb86caadb8cc86bfd44010a8a
Kevin Poorman <kjp@codefriar.com>
2019-09-30 17:22:32 -0400

    socketio works now. that was ... painful

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,22 @@
 import { LightningElement, track } from 'lwc';
+import io from 'socket.io-client';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    socket;
+
+    connectedCallback() {
+        this.initializeSocketIO();
+    }
+
+    initializeSocketIO() {
+        this.socket = io('http://0.0.0.0:3002');
+        console.log('foo');
+        this.socket.on('connect', socket => {
+            console.log('connected!');
+
+            this.socket.on('newCaseInfo', data => {
+                console.log(data);
+            });
+        });
+    }
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3002, () => console.log('running on 3001'));
+
 /*
  * Salesforce org connection.
  * parses the .env file for user/pass/security token and generates a connection object.
  */
 const jsforce = require('jsforce');
 require('dotenv').config();
+
 const { SF_USERNAME, SF_PASSWORD, SF_TOKEN, SF_LOGIN_URL } = process.env;
 if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
     console.error(
@@ -19,9 +26,13 @@ conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, err => {
         console.error(err);
         process.exit(-1);
     }
+    conn.streaming.topic('CaseUpdates').subscribe(function(message) {
+        console.log('Event Type : ' + message.event.type);
+        console.log('Event Created : ' + message.event.createdDate);
+        console.log('Object Id : ' + message.sobject.Id);
+        io.emit('newCaseInfo', message);
+    });
 });
 
 // eslint-disable-next-line no-undef
-module.exports = app => {
-    app.jsForceConn = conn;
-};
+//module.exports = app => {};
